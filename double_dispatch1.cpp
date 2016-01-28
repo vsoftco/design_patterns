1988bb484316f668a45b2d9f500351247a7eeb4f
vsoftco <vsoftco@gmail.com>
2016-01-28 18:51:44 -0500

    commit

--- double_dispatch1.cpp
+++ double_dispatch1.cpp
@@ -1,6 +1,7 @@
 // Double dispatching via virtual functions
 
 #include <iostream>
+#include <memory>
 #include <string>
 
 class Cat;
@@ -111,11 +112,15 @@ void play(const IAnimal& first, const IAnimal& second)
 
 int main()
 {
-    Cat cat;
-    Dog dog;
-    Bird bird;
+    std::unique_ptr<IAnimal> upCat{std::make_unique<Cat>()};
+    std::unique_ptr<IAnimal> upDog{std::make_unique<Dog>()};
+    std::unique_ptr<IAnimal> upBird{std::make_unique<Bird>()};
 
-    play(cat, dog);
-    play(dog, cat);
-    play(bird, cat);
+    play(*upCat, *upDog);
+    play(*upCat, *upBird);
+    play(*upDog, *upBird);
+
+    play(*upDog, *upCat);
+    play(*upBird, *upCat);
+    play(*upBird, *upDog);
 }
